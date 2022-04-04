b757989be50174a10e52ed8d48242965738cc0bb
khalid ansari <khalidansari1002@gmail.com>
2022-04-04 15:12:34 +0530

    Fixed to run sync

--- src/runner/check-table.ts
+++ src/runner/check-table.ts
@@ -1,14 +1,12 @@
-import { config } from "../config/config";
 import { runSql } from "./run-sql";
 
-export const checkTable = (tableList: Array<string>, connection: any) => {
-  tableList.forEach(async (table) => {
-    const checkCountry = `SELECT * FROM information_schema.tables WHERE table_schema = '${config.database}' AND table_name = '${table}'`;
-    const result = await runSql(checkCountry, connection);
-
-    if (result.length > 0) {
-      console.log(`Table ${table} already exist`);
-      process.exit(0);
-    }
-  });
+export const checkTable = async (tableList: Array<string>, connection: any) => {
+  const results = await runSql("SHOW TABLES;", connection);
+  const tableNames = results.map((t) => t[Object.keys(t)[0]]);
+  const duplicateTables = tableList.filter((table) => tableNames.includes(table));
+  if (duplicateTables.length > 0) {
+    console.log(`Tables: ${duplicateTables.join(", ")} already exist`);
+    process.exit(1);
+  }
+  return true;
 };
--- src/runner/create-table.ts
+++ src/runner/create-table.ts
@@ -2,13 +2,18 @@ import { getTableSql } from "../table-structure";
 import { runSql } from "./run-sql";
 
 export const createTable = async (tables: Array<string>, connection: any) => {
-  tables.forEach(async (table) => {
-    const sql = getTableSql(table);
-    try {
-      await runSql(sql, connection);
-      console.log(`Table ${table} created`);
-    } catch (error) {
-      console.log(`Failed to create table ${table}: ${error}`);
-    }
+  return new Promise((resolve) => {
+    tables.forEach(async (table, idx) => {
+      const sql = getTableSql(table);
+      try {
+        await runSql(sql, connection);
+        console.log(`Table ${table} created`);
+        if (idx === tables.length - 1) {
+          resolve(null);
+        }
+      } catch (error) {
+        console.log(`Failed to create table ${table}: ${error}`);
+      }
+    });
   });
 };
