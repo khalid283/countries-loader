a1ab08da36b57795e0f3036b11062b79f255c3c0
khalid ansari <khalidansari1002@gmail.com>
2022-04-04 15:14:21 +0530

    Added more console

--- src/runner/loader.ts
+++ src/runner/loader.ts
@@ -1,26 +1,35 @@
 import { getConnection } from "../connection/connection";
+import { loadCountries } from "../country-downloader";
 import { checkTable } from "./check-table";
 import { createTable } from "./create-table";
+import { insertData } from "./insert-data";
 
 export const loader = async () => {
   /**
    * Check and create connection
    */
+  console.log("Checking connection...");
   const connection = await getConnection();
   if (connection instanceof Error) {
     console.log(connection);
     process.exit(1);
   }
-  /**
-   * Check table exist or not
-   */
+  console.log("Connection established");
+
+  // Check if table
   await checkTable(["countries", "states", "cities"], connection);
 
-  /**
-   * Create table
-   */
-  await createTable(["countries"], connection);
+  // Create table
+  await createTable(["countries", "states", "cities"], connection);
 
-  //   connection.end();
-  //   process.exit(0);
+  // Load countries
+  console.log("Downloading countries...");
+  const countries = await loadCountries();
+  console.log("Countries downloaded successfully");
+
+  console.log("Inserting countries...");
+  await insertData(countries, connection);
+
+  connection.end();
+  process.exit(0);
 };
