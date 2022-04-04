e6615097248f6731a14cc52b70a0fbded05e796a
khalid ansari <khalidansari1002@gmail.com>
2022-04-04 15:13:36 +0530

    Added execute sql method

--- src/runner/run-sql.ts
+++ src/runner/run-sql.ts
@@ -1,4 +1,4 @@
-import { Connection } from "mysql2";
+import { Connection, ResultSetHeader } from "mysql2";
 /**
  * run sql in mysql
  * @param sql
@@ -8,6 +8,18 @@ export const runSql = (sql: string, connection: Connection): Promise<any> => {
     connection.query(sql, (err, result) => {
       if (err) {
         reject(err);
+      } else {
+        resolve(result);
+      }
+    });
+  });
+};
+
+export const executeSql = (sql: string, connection: Connection): Promise<any> => {
+  return new Promise((resolve, reject) => {
+    connection.execute(sql, (err, result: ResultSetHeader) => {
+      if (err) {
+        throw new Error(`Failed to execute sql: ${err}`);
       } else {
         resolve(result);
       }
