77e490659f94333ce0b9a65a18ffb0195485b6e3
khalid ansari <khalidansari1002@gmail.com>
2022-04-04 15:14:01 +0530

    Fixed sync behaviour

--- src/runner/insert-data.ts
+++ src/runner/insert-data.ts
@@ -1,33 +1,36 @@
 /* eslint-disable camelcase */
 import { pick } from "lodash";
-import { Connection, ResultSetHeader } from "mysql2";
+import { Connection } from "mysql2";
 import {
   checkCountryExist,
   checkStateExist,
   getCityDump,
   countryColumnNames,
   stateColumnNames,
+  loadDBCountries,
+  loadDBStates,
 } from "../utils";
 
 import { jsonToSqlInsert } from "../json-sql/json-sql.converter";
+import { executeSql } from "./run-sql";
 
 export const insertData = async (countries, connection: Connection) => {
+  const countryListDB = await loadDBCountries(connection);
   for (const country of countries) {
     const countryData = pick(country, countryColumnNames);
     try {
-      const checkCountry = await checkCountryExist(countryData, connection);
+      const checkCountry = checkCountryExist(countryData, countryListDB);
       if (checkCountry) {
         await insertState(country.states, checkCountry.id, connection);
       } else {
         const sql = jsonToSqlInsert("countries", [countryData]);
-        connection.execute(sql, async (err, result: ResultSetHeader) => {
-          if (err) {
-            throw new Error(`Failed to insert country ${country.name}: ${err}`);
-          }
-          await insertState(country.states, result.insertId, connection);
-          console.log(`Inserted country ${country.name}`);
-        });
+        const result = await executeSql(sql, connection);
+        if (result instanceof Error) {
+          process.exit(1);
+        }
+        await insertState(country.states, result.insertId, connection);
       }
+      console.log(`Inserted successfully country ${country.name}`);
     } catch (error) {
       console.log(`Failed to insert country ${country.name}: ${error}`);
     }
@@ -35,24 +38,21 @@ export const insertData = async (countries, connection: Connection) => {
 };
 
 const insertState = async (states, id, connection: Connection) => {
+  const stateListDB = await loadDBStates(id, connection);
   for (const state of states) {
     const stateData = pick(state, stateColumnNames);
     stateData.country_id = id;
     try {
-      const checkState = await checkStateExist(stateData, id, connection);
+      const checkState = checkStateExist(stateData, id, stateListDB);
       if (checkState) {
         await insertCities(state.cities, checkState.id, id, connection);
       } else {
         const sql = jsonToSqlInsert("states", [stateData]);
-        return new Promise((resolve, reject) => {
-          connection.execute(sql, async (err, result: ResultSetHeader) => {
-            if (err) {
-              throw new Error(`Failed to insert state ${state.name}: ${err}`);
-            }
-            await insertCities(state.cities, result.insertId, id, connection);
-            resolve(result);
-          });
-        });
+        const result = await executeSql(sql, connection);
+        if (result instanceof Error) {
+          process.exit(1);
+        }
+        await insertCities(state.cities, result.insertId, id, connection);
       }
     } catch (error) {
       throw new Error(`Failed to insert state ${state.name}: ${error}`);
@@ -61,18 +61,16 @@ const insertState = async (states, id, connection: Connection) => {
 };
 
 const insertCities = async (cities, state_id, country_id, connection: Connection) => {
-  try {
-    const cityList = getCityDump(cities, state_id, country_id);
-    const sql = jsonToSqlInsert("cities", cityList);
-    return new Promise((resolve, reject) => {
-      connection.execute(sql, (err, result: ResultSetHeader) => {
-        if (err) {
-          throw new Error(`Failed to insert city: ${err}`);
-        }
-        resolve(result);
-      });
-    });
-  } catch (error) {
-    throw new Error(`Failed to insert city: ${error}`);
+  if (cities.length > 0) {
+    try {
+      const cityList = getCityDump(cities, state_id, country_id);
+      const sql = jsonToSqlInsert("cities", cityList);
+      const result = await executeSql(sql, connection);
+      if (result instanceof Error) {
+        process.exit(1);
+      }
+    } catch (error) {
+      throw new Error(`Failed to insert city: ${error}`);
+    }
   }
 };
