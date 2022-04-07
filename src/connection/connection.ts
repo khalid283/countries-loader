bb9320e84b430b12ea6a7a4f4d19b4ad44637fc0
khalid ansari <khalidansari1002@gmail.com>
2022-04-07 21:24:45 +0530

    Fixed for cli env loading

--- src/connection/connection.ts
+++ src/connection/connection.ts
@@ -1,9 +1,16 @@
-import mysql, { Connection } from "mysql2";
-import { config } from "../config/config";
+import * as mysql from "mysql2";
+import { Connection } from "mysql2";
 
 export const getConnection = async (): Promise<Connection> => {
   return new Promise((resolve) => {
-    const connection = mysql.createConnection(config);
+    const connection = mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      port: Number(process.env.DB_PORT) || 3306,
+      charset: process.env.DB_CHARSET,
+    });
     connection.connect((err) => {
       if (err) {
         throw err;
